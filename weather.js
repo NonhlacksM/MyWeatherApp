85c960bd73a31e7a081a467eecd6394c64cb2d96
Nonhlanhla Zwane <nonhlacksmangethe@gmail.com>
2024-04-21 21:36:57 +0200

    weather update

--- weather.js
+++ weather.js
@@ -1,13 +1,29 @@
 //change city and temperature
 function weather(response) {
   //update city
+  console.log(response.data);
   let currentCity = document.querySelector(".current-city");
   let newCity = response.data.city;
   currentCity.innerHTML = newCity;
   //update temp
   let currentTemp = document.querySelector(".current-temperature-value");
+  let currentIcon = document.querySelector(".current-temperature-icon");
   let newTemp = Math.round(response.data.temperature.current);
+  let newIcon = `<img src="${response.data.condition.icon_url}" class ="icon"/>`;
   currentTemp.innerHTML = newTemp;
+  currentIcon.innerHTML = newIcon;
+  //update wind
+  let currentWind = document.querySelector(".current-wind");
+  let newWind = response.data.wind.speed;
+  currentWind.innerHTML = `${newWind}km/h`;
+  // update humidity
+  let currentHumidity = document.querySelector(".current-humidity");
+  let newHumidity = Math.round(response.data.temperature.humidity);
+  currentHumidity.innerHTML = `${newHumidity}%`;
+  //update condition
+  let currentDescription = document.querySelector(".current-description");
+  let newDescription = response.data.condition.description;
+  currentDescription.innerHTML = `, ${newDescription}`;
 }
 // search API
 function searchAPI(city) {
@@ -23,6 +39,7 @@ function searchCity(event) {
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
+searchAPI("jhb");
 //change date
 function formatDate(date) {
   let minutes = date.getMinutes();
