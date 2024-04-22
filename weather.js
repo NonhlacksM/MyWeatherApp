5d3708177016a43908ce6012d13ae704ebe1b577
Nonhlanhla Zwane <nonhlacksmangethe@gmail.com>
2024-04-22 12:18:06 +0200

    forecast update

--- weather.js
+++ weather.js
@@ -1,45 +1,75 @@
 //change city and temperature
 function weather(response) {
   //update city
-  console.log(response.data);
+  console.log(response.data.daily);
   let currentCity = document.querySelector(".current-city");
   let newCity = response.data.city;
   currentCity.innerHTML = newCity;
-  //update temp
-  let currentTemp = document.querySelector(".current-temperature-value");
-  let currentIcon = document.querySelector(".current-temperature-icon");
-  let newTemp = Math.round(response.data.temperature.current);
-  let newIcon = `<img src="${response.data.condition.icon_url}" class ="icon"/>`;
-  currentTemp.innerHTML = newTemp;
-  currentIcon.innerHTML = newIcon;
-  //update wind
-  let currentWind = document.querySelector(".current-wind");
-  let newWind = response.data.wind.speed;
-  currentWind.innerHTML = `${newWind}km/h`;
-  // update humidity
-  let currentHumidity = document.querySelector(".current-humidity");
-  let newHumidity = Math.round(response.data.temperature.humidity);
-  currentHumidity.innerHTML = `${newHumidity}%`;
-  //update condition
-  let currentDescription = document.querySelector(".current-description");
-  let newDescription = response.data.condition.description;
-  currentDescription.innerHTML = `, ${newDescription}`;
+
+  //update forecast
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let now = new Date();
+  let items = document.querySelectorAll("li");
+  items.forEach(function (item, i) {
+    //update temp
+    let currentTemp = document.querySelector(".current-temperature-value");
+    let currentIcon = document.querySelector(".current-temperature-icon");
+    let newTemp = Math.round(response.data.daily[i].temperature.day);
+    let newIcon = `<img src="${response.data.daily[i].condition.icon_url}" class ="icon"/>`;
+    let forecastIcon = item.querySelector(".forecast-temperature-icon");
+    forecastIcon.innerHTML = `<img src="${response.data.daily[i].condition.icon_url}" class ="forecast-temperature-icon"/>`;
+    currentTemp.innerHTML = newTemp;
+    currentIcon.innerHTML = newIcon;
+    //update wind
+    let currentWind = document.querySelector(".current-wind");
+    let newWind = response.data.daily[i].wind.speed;
+    currentWind.innerHTML = `${newWind}km/h`;
+    // update humidity
+    let currentHumidity = document.querySelector(".current-humidity");
+    let newHumidity = Math.round(response.data.daily[i].temperature.humidity);
+    currentHumidity.innerHTML = `${newHumidity}%`;
+    //update condition
+    let currentDescription = document.querySelector(".current-description");
+    let newDescription = response.data.daily[i].condition.description;
+    currentDescription.innerHTML = `, ${newDescription}`;
+    //update the forecast day
+    let day = item.querySelector(".forecast-day");
+    let weekday = days[now.getDay() + i];
+    day.innerHTML = weekday;
+    //update the header icon with the daily forecast icon
+    let header = document.querySelector(".header");
+    header.innerHTML = `<img src="${response.data.daily[i].condition.icon_url}"/>`;
+    //update max temp
+    let maxTemp = item.querySelector(".forecast-temperature-max");
+    maxTemp.innerHTML = `${Math.round(
+      response.data.daily[i].temperature.maximum
+    )}°`;
+    //update min temp
+    let minTemp = item.querySelector(".forecast-temperature-min");
+    minTemp.innerHTML = `${Math.round(
+      response.data.daily[i].temperature.minimum
+    )}°`;
+  });
 }
+
 // search API
 function searchAPI(city) {
   let apiKey = "33bf642o0ct3714a65adf2fc8fed4e85";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(weather);
 }
+
 //search the city from the weather API
 function searchCity(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   searchAPI(searchInputElement.value.trim());
 }
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
 searchAPI("jhb");
+
 //change date
 let days = [
   "Sunday",
